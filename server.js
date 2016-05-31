c67ba0f0703772fccdbba924eb4395e8e1814c1c
Keven Lee <kevenlee123@gmail.com>
2016-05-31 18:49:23 -0400

    Added mocha/chai

--- server.js
+++ server.js
@@ -1,26 +1,67 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var jsonParser = bodyParser.json();
-var chai = require('chai')
-  , chaiHttp = require('chai-http');
 
-chai.use(chaiHttp);
-var Storage = function () {
+var Storage = function() {
     this.items = [];
     this.id = 0;
 };
 
-Storage.prototype.add = function (name) {
-
-    var item = {
-        name: name,
-        id: this.id
-    };
-
+Storage.prototype.add = function(name) {
+    var item = {name: name, id: this.id};
     this.items.push(item);
     this.id += 1;
+    console.log(this.items.indexOf(item));
     return item;
 };
+
+Storage.prototype.delete = function(itemId) {
+    console.log('delete method being called with id=', itemId);
+
+    console.log(this.id);
+    console.log(this.items);
+
+    for (var i=0; i<this.items.length; i++) {
+        console.log(this.items[i]);
+        if (this.items[i].id == itemId) {
+            console.log('Index is: ', i);
+            this.items.splice(i, 1);
+            return {status: 200, data: this.items};
+        }
+    }
+
+    return {status: 204, data: this.items};
+
+};
+
+Storage.prototype.update = function(itemId, content) {
+
+    var exists;
+
+    for (var i=0; i<this.items.length; i++){
+
+        console.log(this.items[i]);
+        if (this.items[i].id == itemId){
+            exists = true;
+            this.items[i].name = content;
+            return {status: 200, data: this.items};
+        }
+        else {
+            exists = false;
+        }
+    }
+
+    if (exists == false){
+        console.log('Falsey falsey!!');
+        var item = {name: content, id: itemId};
+        this.items.push(item);
+        this.id += 1;
+        console.log(this.items.indexOf(item));
+        return {status: 200, data: this.items};
+    }
+
+};
+
 
 var storage = new Storage();
 storage.add('Broad beans');
@@ -30,28 +71,38 @@ storage.add('Peppers');
 var app = express();
 app.use(express.static('public'));
 
-app.get('/items', function (req, res) {
+app.get('/items', function(req, res) {
     res.json(storage.items);
 });
 
-
-app.delete('/items/:id', jsonParser, function (req, res) {
-
-    var index = storage.items.indexOf(req.body.id);
-    storage.items.splice(index, 1);
-    res.json(storage);
-
-});
-
-
-app.post('/items', jsonParser, function (req, res) {
+app.post('/items', jsonParser, function(req, res) {
     if (!req.body) {
         return res.sendStatus(400);
     }
+
     var item = storage.add(req.body.name);
     res.status(201).json(item);
 });
 
+app.delete('/items/:id', function(req, res){
 
+    console.log('Id = ', req.params.id);
+    var item = storage.delete(req.params.id);
+    res.status(item.status).json(item.data);
+
+});
+
+app.put('/items/:id', jsonParser, function(req, res){
+
+    console.log('Id = ', req.params.id);
+    console.log(req.body.name);
+    var item = storage.update(req.params.id, req.body.name);
+    res.status(item.status).json(item.data);
+
+
+});
 
 app.listen(process.env.PORT || 8080);
+
+exports.app = app;
+exports.storage = storage;
--- test/test-server.js
+++ test/test-server.js
@@ -3,13 +3,34 @@ var chaiHttp = require('chai-http');
 var server = require('../server.js');
 
 var should = chai.should();
+var expect = chai.expect;
 var app = server.app;
 var storage = server.storage;
 
 chai.use(chaiHttp);
 
 describe('Shopping List', function() {
-        it('should add an item on POST', function(done) {
+    it('should list items on get', function(done){
+        chai.request(app)
+            .get('/items')
+            .end(function(err, res){
+                should.equal(err, null);
+                res.should.have.status(200);
+                res.should.be.json;
+                res.body.should.be.a('array');
+                res.body.should.have.length(3);
+                res.body[0].should.be.a('object');
+                res.body[0].should.have.property('id');
+                res.body[0].should.have.property('name');
+                res.body[0].id.should.be.a('number');
+                res.body[0].name.should.be.a('string');
+                res.body[0].name.should.equal('Broad beans');
+                res.body[1].name.should.equal('Tomatoes');
+                res.body[2].name.should.equal('Peppers');
+                done();
+            });
+    });
+    it('should add an item on POST', function(done) {
         chai.request(app)
             .post('/items')
             .send({'name': 'Kale'})
@@ -34,8 +55,42 @@ describe('Shopping List', function() {
                 done();
             });
     });
+    it('should edit an item on put', function(done){
+        chai.request(app)
+            .put('/items/1')
+            .send({'name': 'orange'})
+            .end(function(err, res){
+                expect(storage.items[1].name).equal('orange');
+                expect(storage.items[1].id).be.a('number');
+                expect(res).have.status(200);
+                done();
+            });
+        chai.request(app)
+            .put('/items/25')
+            .send({'name':'hello world'})
+            .end(function(err, res){
+                expect(storage.items[24].name).equal('hello world');
+                expect(storage.items[24].id).be.a('number');
+                expect(res).have.status(200);
+                done();
+            });
+
+    });
+    it('should delete an item on delete', function(done){
+        chai.request(app)
+            .delete('/items/2')
+            .end(function(err, res){
+                expect(res).have.status(200);
+                expect(storage.items.length).equal(4);
+                done();
+            });
+        chai.request(app)
+            .delete('/items/10')
+            .end(function(err, res){
+                expect(res).have.status(204);
+                expect(storage.items.length).equal(5);
+                done();
+            });
+
+    });
 });
-
-
-exports.app = app;
-exports.storage = storage;
